c227375202e839d38575a42407a31256307c5cde
Lauren Frankovich <frankovister@gmail.com>
2020-04-23 02:46:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,26 +1,28 @@
-var animal = 'dog'
+var animal = 'dog';
 
 function myAnimal() {
   // You should not need to modify this function
-  return animal
+  return animal;
 }
 
 function yourAnimal() {
-  var animal = 'cat'
+  var animal = 'cat';
   return animal;
 }
 
 function add2(n) {
-  const two = 2
+  const two = 2;
   // Feel free to move things around!
   return n + two;
 }
 
 var funkyFunction = function() {
   return function() {
-    return "FUNKY!"
+    return "FUNKY!";
   }
 }
 
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
 // NOTE: you only need to modify the code below this line
+var preFunk = funkyFunction();
+var theFunk = preFunk();
